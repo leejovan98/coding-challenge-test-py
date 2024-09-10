c89827c4c0df26b042defd184fbd95b6777fa602
adhy-p <adhyp02@gmail.com>
2024-09-10 14:52:38 +0800

    Update app.py template

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
-from flask import Flask
+import logging
+import socket
 
-app = Flask(__name__)
+from routes import app
 
-
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+logger = logging.getLogger(__name__)
 
 
-if __name__ == '__main__':
-    app.run()
+@app.route("/", methods=["GET"])
+def default_route():
+    return "Python Template"
+
+
+logger = logging.getLogger()
+handler = logging.StreamHandler()
+formatter = logging.Formatter("%(asctime)s %(name)-12s %(levelname)-8s %(message)s")
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
+
+if __name__ == "__main__":
+    logging.info("Starting application ...")
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind(("localhost", 8080))
+    port = sock.getsockname()[1]
+    sock.close()
+    app.run(port=port)
